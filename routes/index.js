fd1799b4065d81facb889286536fe075b268259d
Adil fareed <82055942+Adilfareed@users.noreply.github.com>
2023-12-05 19:51:03 +0500

    created routes and home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var usermodel = require("./users");
+const session = require("express-session");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+passport.authenticate(new localStrategy(usermodel.authenticate()));
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res) {
+  res.render("index");
 });
+router.get("/profile", isLoggedIn, function (req, res) {
+  res.send("this is profile");
+});
+router.post("/register", function (req, res) {
+  var userdata = new usermodel({
+    username: req.body.username,
+    email: req.body.email,
+    fullname : req.body.fullname,
+   
+    
+  });
+  
+  usermodel.register(userdata, req.body.password)
+    .then(function (registereduser) {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/profile");
+      });
+    });
+});
+router.post("/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/",
+  }),
+  function (req, res) {}
+);
+router.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://127.0.0.1:27017/pinterestdb");
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, },
+  fullname : { type: String, required: true },
 });
+userSchema.plugin(plm);
 
-module.exports = router;
+module.exports = mongoose.model("User", userSchema);
