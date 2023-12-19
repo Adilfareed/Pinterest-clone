0b307d242f4b0c1beee3bd0ae51430201354169e
Adil fareed <82055942+Adilfareed@users.noreply.github.com>
2023-12-19 19:24:53 +0500

    updated app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const flash = require("connect-flash");
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
 
 app.use(flash());
