f8a4f821e259e2911f6ea828e45329af8085f66f
Adil fareed <82055942+Adilfareed@users.noreply.github.com>
2023-12-23 19:30:06 +0500

    connected with mongodb atlas

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,38 @@
-var express = require("express");
-var router = express.Router();
+// Load environment variables from .env file
+require('dotenv').config();
+
+const express = require("express");
+const router = express.Router();
 const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/pinterestdb");
+// Use environment variables for sensitive information
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Use async/await for cleaner code structure
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to the MongoDB database");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+}
+
+connectToDatabase();
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, },
+  password: { type: String },
   profileimage: { type: String },
-  fullname : { type: String, required: true },
-  posts : [{ type: mongoose.Schema.Types.ObjectId,
-  ref: "Post",}]
+  fullname: { type: String, required: true },
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
 });
+
 userSchema.plugin(plm);
 
 module.exports = mongoose.model("User", userSchema);
